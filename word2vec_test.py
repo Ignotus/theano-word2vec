86a710e9a63e92a5d8072633552766e059c7afd4
Minh Ngo <nlminhtl@gmail.com>
2016-05-25 21:31:14 +0200

    Adding word2vec_test.py

---
+++ word2vec_test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+import sys
+import cPickle as pickle
+from word2vec import *
+
+
+if __name__ == '__main__':
+    corpus = Corpus('data/', vocab_size=8000, corpus_file='corpus')
+
+    vector_size = 150
+    context_window_size = 5
+    # https://groups.google.com/forum/#!msg/word2vec-toolkit/O9YUT0p5rCw/LIjsunElNm4J
+    epochs = 10
+    learning_rate = 0.05
+    batch_size = 2
+
+    skip_gram = (len(sys.argv) == 1 or sys.argv[1] == '--skip_gram')
+
+    if skip_gram:
+        trainer = SkipGram(vector_size, corpus)
+        prefix = 'skip_gram'
+    else:
+        trainer = CBOW(vector_size, corpus)
+        prefix = 'cbow'
+    loss, loss_changes = trainer.train(context_window_size, learning_rate, epochs, batch_size)
+
+    trainer.save('%s.npy' % prefix)
+    with open('%s_losses.npy' % prefix, 'w') as f:
+        pickle.dump(loss_changes, f)
